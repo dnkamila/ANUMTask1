55b0943d177ef3b3c7a10feb0509d58dd3f40a7b
Kamila D. Nabilati <dnkamila@gmail.com>
2015-03-15 00:09:17 +0700

    Reference : fix LDLFact : add separate LDLFactFull : Faktorisasi A menjadi L dan D + Result Ax = b - input : A, b dimana A matriks simetrik (nxn) dan b (nx1) - output : L, D, dan x dimana LDL' = A dan Ax = b

--- LDLFact.m
+++ LDLFact.m
@@ -3,14 +3,14 @@ function [L, D] = LDLFact(A)
     
     L = zeros(n,n);
     for i = 1:n
-        L(i,i)=1;
+        L(i,i) = 1;
     end
     
     D = zeros(n,1);
-    for i=1:n
-        D(i)=A(i,i)-L(i,1:i-1).^2*D(1:i-1);
-        for j=i+1:n
-            L(j,i)=(A(j,i)-L(j,1:i-1).*L(i,1:i-1)*D(1:i-1))/D(i);
+    for i = 1:n
+        D(i) = A(i,i)-L(i,1:i-1).^2*D(1:i-1);
+        for j = i+1:n
+            L(j,i) = (A(j,i)-L(j,1:i-1).*L(i,1:i-1)*D(1:i-1))/D(i);
         end
     end
 end
