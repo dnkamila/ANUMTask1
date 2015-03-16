87a260688a952ea029d3a5e3615a774f1f927706
Kamila D. Nabilati <dnkamila@gmail.com>
2015-03-16 09:34:27 +0700

    Not yet : A(rows,rows) = A(rows,rows) - vv*vv'/alfa; L = tril(A,-1)+eye(n,n); D = diag(diag(A));

---
+++ GetCol.m
@@ -0,0 +1,9 @@
+function [vcol] = GetCol(v, n, row, col)
+%     k = 1;
+%     vcol = zeros(n-col+1-row+1,1);
+%     for i = row:n
+%         vcol(k) = v((n-col/2)*(col-1)+i);
+%         k = k+1;
+%     end
+    100
+end
