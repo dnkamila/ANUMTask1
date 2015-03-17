ac322d018af8a5feb094e50ca370b6ec2f894907
Kamila D. Nabilati <dnkamila@gmail.com>
2015-03-17 07:52:05 +0700

    backward and forward

--- backward.m
+++ backward.m
@@ -1,14 +1,14 @@
 function [x] = backwardOri(U,b)
-%Input	: matriks U dan vector b
-%Output	: vektor solusi x
-n = length( b );
-x = zeros( n, 1 );
-x(n)=b(n)/U(length(U));
-  for i=n-1:-1:1
-      t=0;
-      for j=n:-1:i+1
-          t=t+U((n-i/2)*(i-1)+j)*x(j);
-      end
-      x(i)=(b(i)-t)/U((n-i/2)*(i-1)+i);
-  end
+    n = length( b );
+    x = zeros( n, 1 );
+    
+    x(n)=b(n)/U(length(U));
+    
+    for i=n-1:-1:1
+        t=0;
+        for j=n:-1:i+1
+            t=t+U((n-i/2)*(i-1)+j)*x(j);
+        end
+        x(i)=(b(i)-t)/U((n-i/2)*(i-1)+i);
+    end
 end
